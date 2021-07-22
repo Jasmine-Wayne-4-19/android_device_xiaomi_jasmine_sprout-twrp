e7890fb44132d4cf56e0b6d85fb4558586128230
IMMANUEL44 <immanuelr44@gmail.com>
2021-07-22 16:31:24 +0530

    jasmine_sprout: Adapt to A10 changes

--- omni_jasmine_sprout.mk
+++ omni_jasmine_sprout.mk
@@ -18,7 +18,6 @@
 PRODUCT_RELEASE_NAME := jasmine_sprout
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
